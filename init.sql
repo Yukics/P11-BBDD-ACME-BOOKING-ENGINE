51ccaf99e53b740a9beaa801795adea76f11e428
Alejo <Morell>
2021-05-04 16:34:20 +0200

    Updated tables so it works with our csv

--- init.sql
+++ init.sql
@@ -2,41 +2,66 @@ create user customer01 PASSWORD 'password'; --With this line we create customer0
 comment on role customer01 is 'Bookings owner'; --This is just a comment about customer01
 create database "Bookings" with owner customer01; --We are creating our database and assign it to our user
 
-
 create schema "bookings"; --We create the schema where we will be working on
 comment on schema "bookings" is 'Actual Bookings Schema'; --Just a comment
 alter schema "bookings" owner to customer01; --We set customer01 as the owner of this schema, btw it should already be, because hereditary ownership
 
-create table "bookings"."CUSTOMERS"( --Customers creation table with autoreferenced foreign key
-	id int primary key not null,
-	lastname varchar(40) not null,
-	firstname varchar(40) not null,
-	address varchar(120) not null,
-	zip numeric(5) not null,
-	phonenumber varchar(14) not null,
-	recommended_id int,
-	registerdate timestamp not null,
-	foreign key (recommended_id) references "bookings"."CUSTOMERS" (id) on update cascade
+create table "bookings"."FACILITIES"
+(
+    id               integer     not null
+        constraint "FACILITIES_pkey"
+            primary key,
+    name             varchar(50) not null,
+    cust_cost        money       not null,
+    guest_cost       money       not null,
+    purchase_cost    money       not null,
+    maintenance_cost money       not null
 );
-comment on table "bookings"."CUSTOMERS" is 'People';
 
-create table "bookings"."FACILITIES"( --Facilities table with no relations
-	id int primary key not null,
-	name int not null,
-	cust_cost money not null,
-	guest_cost money not null,
-	purchase_cost money not null,
-	maintenance_cost money not null
-);
-comment on table "bookings"."FACILITIES" is 'What you can reserve';
+comment on table  "bookings"."FACILITIES" is 'What you can reserve';
 
-create table "bookings"."BOOKINGS"( --Bookings table related to customers and facilities
-	fac_id int not null,
-	cust_id int not null,
-	start_datetime timestamp not null,
-	nhours int not null,
-    foreign key (cust_id) references "bookings"."CUSTOMERS" (id) on update cascade,
-    foreign key (fac_id) references "bookings"."FACILITIES" (id) on update cascade,
-    PRIMARY KEY (fac_id, cust_id)
+alter table  "bookings"."FACILITIES"
+    owner to customer01;
+
+create table  "bookings"."CUSTOMERS"
+(
+    id             integer      not null
+        constraint "CUSTOMERS_pkey"
+            primary key,
+    lastname       varchar(40)  not null,
+    firstname      varchar(40)  not null,
+    address        varchar(120) not null,
+    zip            numeric(5)   not null,
+    phonenumber    varchar(14)  not null,
+    recommended_id integer
+        constraint "CUSTOMERS_recommended_id_fkey"
+            references  "bookings"."CUSTOMERS"
+            on update cascade,
+    registerdate   timestamp    not null
 );
-comment on table "bookings"."BOOKINGS" is 'Reservations';
+
+comment on table  "bookings"."CUSTOMERS" is 'People';
+
+alter table  "bookings"."CUSTOMERS"
+    owner to customer01;
+
+create table  "bookings"."BOOKINGS"
+(
+    fac_id         integer   not null
+        constraint "BOOKINGS_fac_id_fkey"
+            references  "bookings"."FACILITIES"
+            on update cascade,
+    cust_id        integer   not null
+        constraint "BOOKINGS_cust_id_fkey"
+            references  "bookings"."CUSTOMERS"
+            on update cascade,
+    start_datetime timestamp not null,
+    nhours         integer   not null,
+    constraint bookings_pk
+        primary key (fac_id, cust_id, start_datetime)
+);
+
+comment on table  "bookings"."BOOKINGS" is 'Reservations';
+
+alter table  "bookings"."BOOKINGS"
+    owner to customer01;
